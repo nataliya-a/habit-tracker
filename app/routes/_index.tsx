fc836b4c1cec75ee6e201c8fa71fbd80fc52081d
nataliya-a <nkarmarkar@dons.usfca.edu>
2024-01-17 10:55:26 -0800

    feat: route change

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import Home from "./home";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,34 +10,35 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="text-center bg-primary text-black font-black text-6xl  p-8">
-      <h1>Welcome to Remix</h1>
+    // <div className="text-center bg-primary text-black font-black text-6xl  p-8">
+    //   <h1>Welcome to Remix</h1>
 
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            bye
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    //   <ul>
+    //     <li>
+    //       <a
+    //         target="_blank"
+    //         href="https://remix.run/tutorials/blog"
+    //         rel="noreferrer"
+    //       >
+    //         bye
+    //       </a>
+    //     </li>
+    //     <li>
+    //       <a
+    //         target="_blank"
+    //         href="https://remix.run/tutorials/jokes"
+    //         rel="noreferrer"
+    //       >
+    //         Deep Dive Jokes App Tutorial
+    //       </a>
+    //     </li>
+    //     <li>
+    //       <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+    //         Remix Docs
+    //       </a>
+    //     </li>
+    //   </ul>
+    // </div>
+    <Home />
   );
 }
