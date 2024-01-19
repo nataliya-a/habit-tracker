564260c1ca6cb354d92343e0a4758225f0c92dc7
nataliya-a <nkarmarkar@dons.usfca.edu>
2024-01-19 13:17:30 -0800

    fix: title and desc

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,8 +3,8 @@ import Home from "./home";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "tractr" },
+    { name: "description", content: "the habit tracker" },
   ];
 };
 
